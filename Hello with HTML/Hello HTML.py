d9666e92efc57b975f0e51672ef91eed284dfa29
file00 <51076009+file00@users.noreply.github.com>
2024-06-04 21:28:14 +0900

    Update Hello HTML.py

--- Hello with HTML/Hello HTML.py
+++ Hello with HTML/Hello HTML.py
@@ -1,8 +1,3 @@
-# Jinja2 Template Engine
-
-# Flask 설치시 같이 설치되기 때문에 추가 설치 X
-# Flask의 Template Files은 기본적으로 Templates Folder에 저장함.
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
