baadd0b3eafcea4ca9823029831a45f10fdda13a
file00 <51076009+file00@users.noreply.github.com>
2024-06-04 21:56:44 +0900

    Update Template Test.py

--- Template Test/Template Test.py
+++ Template Test/Template Test.py
@@ -3,7 +3,7 @@ app  = Flask(__name__)
 
 @app.route('/')
 def temp_test():
-    return render_template('temp.html', my_string="템플릿 테스트", my_list=[11,22,33,44,55,66,77])
+    return render_template('Template Test.html', my_string="Template Test", my_list=[11,22,33,44,55,66,77])
 
 if __name__ == "__main__":
     app.run()
