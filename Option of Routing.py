3b54e3015d6582d521ce66fa8346b7266f3bcd2a
file00 <51076009+file00@users.noreply.github.com>
2024-06-07 09:42:14 +0900

    Add files via upload

---
+++ Option of Routing.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/aaa",defaults={ "bbs_id":100 })
+@app.route("/aaa/<bbs_id>")
+def aaa(bbs_id):
+    return "This is {} from 'aaa'.".format(bbs_id)
+
+if __name__ == "__main__":
+    app.run()
+    
